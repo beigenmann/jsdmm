201ec2c9e1cf804f02a93e81e334f9b483750581
Benno Eigenmann <beigenmann@gmail.com>
2017-12-28 12:56:05 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { DmmComponent } from './dmm/dmm.component';
-
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +16,7 @@ import { DmmComponent } from './dmm/dmm.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/js/logic.js
@@ -0,0 +1,47 @@
+var connectionId = -1;
+
+function onConnect(connectionInfo) {
+  if (!connectionInfo) {
+    setStatus('Could not open');
+    return;
+  }
+  connectionId = connectionInfo.connectionId;
+  setStatus('Connected');
+
+};
+
+function setStatus(status) {
+  document.getElementById('status').innerText = status;
+}
+
+function buildPortPicker(ports) {
+  var eligiblePorts = ports.filter(function(port) {
+    return !port.path.match(/[Bb]luetooth/);
+  });
+
+  var portPicker = document.getElementById('port-picker');
+  eligiblePorts.forEach(function(port) {
+    var portOption = document.createElement('option');
+    portOption.value = portOption.innerText = port.path;
+    portPicker.appendChild(portOption);
+  });
+
+  portPicker.onchange = function() {
+    if (connectionId != -1) {
+      chrome.serial.disconnect(connectionId, openSelectedPort);
+      return;
+    }
+    openSelectedPort();
+  };
+}
+
+function openSelectedPort() {
+  var portPicker = document.getElementById('port-picker');
+  var selectedPort = portPicker.options[portPicker.selectedIndex].value;
+  chrome.serial.connect(selectedPort, onConnect);
+}
+
+onload = function() {
+ 
+};
+
