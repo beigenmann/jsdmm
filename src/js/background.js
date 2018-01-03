199939af6e6641d18e340b634fc10cc3c2e2116f
Benno Eigenmann <beigenmann@gmail.com>
2018-01-03 13:51:17 +0100

    init

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,4 @@
 chrome.app.runtime.onLaunched.addListener(function () {
-    chrome.serial.getDevices(function (devices) {
-        console.log("onSetControlSignals: " + devices);
-    });
     chrome.app.window.create('index.html',
         {
             frame: 'Digital Voltmeter',
