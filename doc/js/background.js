5329a85f1682f92f92e00d3e384675915d6bd49d
Benno Eigenmann <beigenmann@gmail.com>
2018-01-15 07:55:19 +0100

    init

--- doc/js/background.js
+++
@@ -1,8 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function () {
-    chrome.app.window.create('index.html',
-        {
-            frame: 'Digital Voltmeter',
-            id: "mainwin",
-            innerBounds: { width: 600, height: 600 }
-        });
-});
